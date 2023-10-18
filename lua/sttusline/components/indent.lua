6d7bfdae2552c76fd60a0a60f949ff8f83e25f2e
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-18 12:27:05 +0700

    chore: simplify indent component

--- lua/sttusline/components/indent.lua
+++ lua/sttusline/components/indent.lua
@@ -5,9 +5,6 @@ Indent.set_colors { fg = colors.cyan, bg = colors.bg }
 
 Indent.set_event("BufEnter")
 
-Indent.set_update(function()
-	local tab_count = vim.api.nvim_buf_get_option(0, "shiftwidth") .. ""
-	return "Tab: " .. tab_count
-end)
+Indent.set_update(function() return "Tab: " .. vim.api.nvim_buf_get_option(0, "shiftwidth") .. "" end)
 
 return Indent
