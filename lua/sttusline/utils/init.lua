d83ad68473fd99b543d2436229428b87ce7bbef7
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-19 18:15:54 +0700

    perl: remove conditions that are never checked

--- lua/sttusline/utils/init.lua
+++ lua/sttusline/utils/init.lua
@@ -9,15 +9,12 @@ M.add_padding = function(str, value)
 
 	if type(value) == "number" then
 		if value <= 0 then return str end
-
 		local padding = (" "):rep(value)
 
 		local startpos = str:find([[#([^#%%]+)%%%*]])
-		if not startpos or not vim.endswith(str, "%*") then
-			return padding .. str .. padding
-		else
-			return str:sub(1, startpos) .. padding .. str:sub(startpos + 1, #str - 2) .. padding .. "%*"
-		end
+		if not startpos then return padding .. str .. padding end
+
+		return str:sub(1, startpos) .. padding .. str:sub(startpos + 1, #str - 2) .. padding .. "%*"
 	elseif type(value) == "table" then
 		local left_padding = type(value.left) == "number" and value.left >= 0 and (" "):rep(value.left)
 			or " "
@@ -25,15 +22,12 @@ M.add_padding = function(str, value)
 			or " "
 
 		local startpos = str:find([[#([^#%%]+)%%%*]])
-		if not startpos or not vim.endswith(str, "%*") then
-			return left_padding .. str .. right_padding
-		else
-			return str:sub(1, startpos)
-				.. left_padding
-				.. str:sub(startpos + 1, #str - 2)
-				.. right_padding
-				.. "%*"
-		end
+		if not startpos then return left_padding .. str .. right_padding end
+		return str:sub(1, startpos)
+			.. left_padding
+			.. str:sub(startpos + 1, #str - 2)
+			.. right_padding
+			.. "%*"
 	end
 end
 
