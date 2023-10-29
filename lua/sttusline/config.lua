d037dd2dcea46bcec5e40f1676b8e72c070e17f6
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-29 08:25:22 +0700

    perl: improve apppl_user_config func

--- lua/sttusline/config.lua
+++ lua/sttusline/config.lua
@@ -58,7 +58,17 @@ end
 M.apply_user_config = function(opts)
 	if type(opts) == "table" then
 		for k, v in pairs(opts) do
-			configs[k] = v
+			if type(v) == type(configs[k]) then
+				if type(v) == "table" then
+					if v[1] == nil then
+						for k2, v2 in pairs(v) do
+							if type(v2) == type(configs[k][k2]) then configs[k][k2] = v2 end
+						end
+					end
+				else
+					configs[k] = v
+				end
+			end
 		end
 	end
 	return configs
