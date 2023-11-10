c2dd45cd6917a1d70f9a46da0f7f489c6ade98ff
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-11-10 16:26:13 +0700

    perl: improve copilot loading by using copilot.api

--- lua/sttusline/components/copilot-loading.lua
+++ lua/sttusline/components/copilot-loading.lua
@@ -1,71 +1,55 @@
 return {
-	name = "copilot",
-	timing = 500,
-	space = function(configs, component)
-		local require = require
-		local pcall = pcall
-		local copilot_status = ""
-		local copilot_client = nil
-		local copilot_handler_registered = false
-		local S = {}
-
-		S.check_status = function()
-			local cp_client_ok, cp_client = pcall(require, "copilot.client")
-			if not cp_client_ok then
-				require("sttusline.utils.notify").error("Cannot load copilot.client")
-				copilot_status = "error"
-				return
-			end
-
-			copilot_client = cp_client.get()
-
-			if not copilot_client then
-				copilot_status = "error"
-				return
-			end
-
-			local cp_api_ok, cp_api = pcall(require, "copilot.api")
-			if not cp_api_ok then
-				copilot_status = "error"
-				require("sttusline.utils.notify").error("Cannot load copilot.api")
-				return
-			end
-
-			cp_api.check_status(copilot_client, {}, function(cserr, status)
-				if
-					cserr
-					or not status.user
-					or status.status == "NoTelemetryConsent"
-					or status.status == "NotAuthorized"
-				then
-					copilot_status = "error"
-					return
-				end
-
-				copilot_status = cp_client.buf_is_attached(0) and "normal" or "error"
-			end)
-		end
-
-		S.register_status_notification_handler = function()
-			if not copilot_handler_registered then
+	name = "copilot-loading",
+	user_event = {
+		"CopilotStatusNormal",
+		"CopilotStatusError",
+		"CopilotStatusInProgress",
+		"CopilotStatusWarning",
+		"CopilotStatusUnknow",
+	},
+	init = function(configs)
+		local nvim_exec_autocmds = vim.api.nvim_exec_autocmds
+		local schedule = vim.schedule
+		local g = vim.g
+		local sttusline_copilot_timer = vim.loop.new_timer()
+		vim.api.nvim_create_autocmd("InsertEnter", {
+			once = true,
+			desc = "Init copilot status",
+			callback = function()
 				local cp_api_ok, cp_api = pcall(require, "copilot.api")
 				if cp_api_ok then
-					cp_api.register_status_notification_handler(
-						function(data) copilot_status = string.lower(data.status) end
-					)
-					copilot_handler_registered = true
+					cp_api.register_status_notification_handler(function(data)
+						schedule(function()
+							g.sttusline_copilot_status = string.lower(data.status or "")
+							if g.sttusline_copilot_status == "normal" then
+								nvim_exec_autocmds("User", { pattern = "CopilotStatusNormal", modeline = false })
+							elseif g.sttusline_copilot_status == "error" then
+								nvim_exec_autocmds("User", { pattern = "CopilotStatusError", modeline = false })
+							elseif g.sttusline_copilot_status == "inprogress" then
+								sttusline_copilot_timer:start(
+									0,
+									configs.loading_speed,
+									vim.schedule_wrap(
+										function()
+											nvim_exec_autocmds(
+												"User",
+												{ pattern = "CopilotStatusInProgress", modeline = false }
+											)
+										end
+									)
+								)
+								return
+							elseif g.sttusline_copilot_status == "warning" then
+								nvim_exec_autocmds("User", { pattern = "CopilotStatusWarning", modeline = false })
+							else
+								nvim_exec_autocmds("User", { pattern = "CopilotStatusUnknow", modeline = false })
+							end
+							sttusline_copilot_timer:stop()
+						end)
+					end)
 				end
-			end
-		end
-		S.get_status = function()
-			local icon = configs.icons[copilot_status]
-			if copilot_status == "inprogress" then
-				return icon[math.floor(vim.loop.hrtime() / 1000000 / component.timing) % #icon + 1]
-			else
-				return icon or copilot_status or ""
-			end
-		end
-		return S
+			end,
+		})
 	end,
 	configs = {
 		icons = {
@@ -74,12 +58,58 @@ return {
 			warning = "",
 			inprogress = { "", "󰪞", "󰪟", "󰪠", "󰪢", "󰪣", "󰪤", "󰪥" },
 		},
+		loading_speed = 200, -- ms
 	},
-	update = function(_, space)
+	update = function(configs)
+		local g = vim.g
+
 		if package.loaded["copilot"] then
-			space.register_status_notification_handler()
-			space.check_status()
+			local cp_client_ok, cp_client = pcall(require, "copilot.client")
+
+			if not cp_client_ok then
+				g.sttusline_copilot_status = "error"
+				require("sttusline.utils.notify").error("Cannot load copilot.client")
+				return
+			end
+
+			local copilot_client = cp_client.get()
+
+			if not copilot_client then
+				g.sttusline_copilot_status = "error"
+				return
+			end
+
+			local cp_api_ok, cp_api = pcall(require, "copilot.api")
+
+			if not cp_api_ok then
+				g.sttusline_copilot_status = "error"
+				require("sttusline.utils.notify").error("Cannot load copilot.api")
+				return
+			end
+
+			cp_api.check_status(copilot_client, {}, function(cserr, status)
+				if cserr or not status.user or status.status ~= "OK" then
+					g.sttusline_copilot_status = "error"
+					return
+				end
+				-- if
+				-- 	cserr
+				-- 	or not status.user
+				-- 	or status.status == "NoTelemetryConsent"
+				-- 	or status.status == "NotAuthorized"
+				-- 	or not cp_client.buf_is_attached(0)
+				-- then
+				-- 	g.copilot_status = "error"
+				-- 	return
+				-- end
+			end)
 		end
-		return space.get_status()
+
+		local icon = configs.icons[g.sttusline_copilot_status]
+		if g.sttusline_copilot_status == "inprogress" then
+			return icon[math.floor(vim.loop.hrtime() / 1000000 / configs.loading_speed) % #icon + 1]
+		else
+			return icon or g.sttusline_copilot_status or ""
+		end
 	end,
 }
--- lua/sttusline/components/copilot.lua
+++ lua/sttusline/components/copilot.lua
@@ -1,3 +1,5 @@
+local copilot_status = ""
+
 return {
 	name = "copilot",
 	user_event = {
@@ -18,7 +20,6 @@ return {
 	init = function()
 		local nvim_exec_autocmds = vim.api.nvim_exec_autocmds
 		local schedule = vim.schedule
-		local g = vim.g
 		vim.api.nvim_create_autocmd("InsertEnter", {
 			once = true,
 			desc = "Init copilot status",
@@ -27,14 +28,14 @@ return {
 				if cp_api_ok then
 					cp_api.register_status_notification_handler(function(data)
 						schedule(function()
-							g.copilot_status = string.lower(data.status or "")
-							if g.copilot_status == "normal" then
+							copilot_status = string.lower(data.status or "")
+							if copilot_status == "normal" then
 								nvim_exec_autocmds("User", { pattern = "CopilotStatusNormal", modeline = false })
-							elseif g.copilot_status == "error" then
+							elseif copilot_status == "error" then
 								nvim_exec_autocmds("User", { pattern = "CopilotStatusError", modeline = false })
-							elseif g.copilot_status == "inprogress" then
+							elseif copilot_status == "inprogress" then
 								nvim_exec_autocmds("User", { pattern = "CopilotStatusInProgress", modeline = false })
-							elseif g.copilot_status == "warning" then
+							elseif copilot_status == "warning" then
 								nvim_exec_autocmds("User", { pattern = "CopilotStatusWarning", modeline = false })
 							else
 								nvim_exec_autocmds("User", { pattern = "CopilotStatusUnknow", modeline = false })
@@ -46,11 +47,10 @@ return {
 		})
 	end,
 	update = function(configs)
-		local g = vim.g
 		if package.loaded["copilot"] then
 			local cp_client_ok, cp_client = pcall(require, "copilot.client")
 			if not cp_client_ok then
-				g.copilot_status = "error"
+				copilot_status = "error"
 				require("sttusline.utils.notify").error("Cannot load copilot.client")
 				return
 			end
@@ -58,30 +58,33 @@ return {
 			local copilot_client = cp_client.get()
 
 			if not copilot_client then
-				g.copilot_status = "error"
+				copilot_status = "error"
 				return
 			end
 
 			local cp_api_ok, cp_api = pcall(require, "copilot.api")
 			if not cp_api_ok then
-				g.copilot_status = "error"
+				copilot_status = "error"
 				require("sttusline.utils.notify").error("Cannot load copilot.api")
 				return
 			end
 			cp_api.check_status(copilot_client, {}, function(cserr, status)
-				if
-					cserr
-					or not status.user
-					or status.status == "NoTelemetryConsent"
-					or status.status == "NotAuthorized"
-				then
-					g.copilot_status = "error"
+				if cserr or not status.user or status.status ~= "OK" or not cp_client.buf_is_attached(0) then
+					copilot_status = "error"
 					return
 				end
-
-				g.copilot_status = cp_client.buf_is_attached(0) and "normal" or "error"
+				-- if
+				-- 	cserr
+				-- 	or not status.user
+				-- 	or status.status == "NoTelemetryConsent"
+				-- 	or status.status == "NotAuthorized"
+				--  or not cp_client.buf_is_attached(0)
+				-- then
+				-- 	copilot_status = "error"
+				-- 	return
+				-- end
 			end)
 		end
-		return configs.icons[g.copilot_status] or g.copilot_status or ""
+		return configs.icons[copilot_status] or copilot_status or ""
 	end,
 }
