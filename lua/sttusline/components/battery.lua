636c3f034ceddc869edee6585291f1db71cc7053
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-11-14 07:55:04 +0700

    fix: update battery component

--- lua/sttusline/components/battery.lua
+++ lua/sttusline/components/battery.lua
@@ -1,5 +1,6 @@
 local fn = vim.fn
 local colors = require("sttusline.utils.color")
+local current_charging_index = 0
 
 return {
 	name = "battery",
@@ -38,11 +39,12 @@ return {
 		fg = colors.green,
 	},
 	space = function()
-		local bat = fn.system("ls /sys/class/power_supply/ | grep BAT"):match("(.-)%s*$") or ""
-		local bat_dir = "/sys/class/power_supply/" .. bat .. "/"
+		local bat_dir = fn.glob("/sys/class/power_supply/BAT*", true, true)[1]
+		if not bat_dir then return "" end
+		bat_dir = bat_dir:match("(.-)%s*$")
 
 		local read_battery_file = function(filename)
-			local f = io.open(bat_dir .. filename, "r")
+			local f = io.open(bat_dir .. "/" .. filename, "r")
 			if not f then return "" end
 			local content = f:read("*all")
 			f:close()
@@ -51,7 +53,6 @@ return {
 		return {
 			get_status = function() return read_battery_file("status") end,
 			get_capacity = function() return read_battery_file("capacity") end,
-			curr_charging_index = 0,
 		}
 	end,
 	update = function(configs, space)
@@ -59,22 +60,22 @@ return {
 		local capacity = space.get_capacity()
 		local icon_index = math.floor(capacity / 10) + 1
 		local battery_color = icon_index > 8 and colors.green
-			or icon_index > 4 and colors.yellow
+			or icon_index > 3 and colors.yellow
 			or colors.red
 
 		if status == "Charging" then
-			space.curr_charging_index = space.curr_charging_index == 0 and icon_index
-				or space.curr_charging_index < #configs.icons.charging and space.curr_charging_index + 1
+			current_charging_index = current_charging_index == 0 and icon_index
+				or current_charging_index < #configs.icons.charging and current_charging_index + 1
 				or icon_index
 
 			return {
 				{
-					configs.icons.charging[space.curr_charging_index] .. " " .. capacity .. "%%",
+					configs.icons.charging[current_charging_index] .. " " .. capacity .. "%%",
 					{ fg = battery_color },
 				},
 			}
-		elseif status == "Discharging" then
-			space.curr_charging_index = 0
+		elseif status == "Discharging" or status == "Not charging" then
+			current_charging_index = 0
 			return {
 				{
 					configs.icons.discharging[icon_index] .. " " .. capacity .. "%%",
@@ -82,13 +83,15 @@ return {
 				},
 			}
 		elseif status == "Full" then
-			space.curr_charging_index = 0
-			return "󰂄 " .. capacity .. "%%"
-		elseif status == "Not charging" then
-			space.curr_charging_index = 0
-			return "󰁹 " .. capacity .. "%%"
+			current_charging_index = 0
+			return {
+				{
+					"󰂄 " .. capacity .. "%%",
+					{ fg = battery_color },
+				},
+			}
 		else
-			space.curr_charging_index = 0
+			current_charging_index = 0
 			return "Battery: " .. capacity .. "%%"
 		end
 	end,
