7cfaecd1db680f978e697e57b9062651ae2040d2
marcelarie <googlillo@gmail.com>
2024-02-01 17:25:14 +0100

    feat: backup to filetype string if nvim-web-devicons is not installed

--- lua/sttusline/components/file-type.lua
+++ lua/sttusline/components/file-type.lua
@@ -11,11 +11,17 @@ return {
 	padding = 2,
 	user_event = "VeryLazy",
 	update = function()
-		local icon = require("nvim-web-devicons").get_icon(
-			vim.fn.expand("%:t"),
-			vim.fn.expand("%:e"),
-			{ default = true }
-		)
-		return string.format("%s", icon)
+		local has_devicons, web_devicons = pcall(require, "nvim-web-devicons")
+		local ft
+
+		if has_devicons then
+			ft = web_devicons.get_icon(vim.fn.expand("%:t"), vim.fn.expand("%:e"), {
+				default = true,
+			})
+		else
+			ft = vim.bo.filetype
+		end
+
+		return string.format("%s", ft)
 	end,
 }
