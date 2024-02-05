c98939c71e3d66aad7405e73f32a109ef4bb162b
marcelarie <googlillo@gmail.com>
2024-02-05 12:26:39 +0100

    refactor: naming

--- lua/sttusline/components/file-type.lua
+++ lua/sttusline/components/file-type.lua
@@ -11,19 +11,19 @@ return {
 	padding = 2,
 	user_event = "VeryLazy",
 	update = function()
-		local ft, icon_highlight
+		local ft, ft_highlight
 		local has_web_devicons, web_devicons = pcall(require, "nvim-web-devicons")
 
 		if has_web_devicons then
-			ft, icon_highlight =
+			ft, ft_highlight =
 				web_devicons.get_icon(vim.fn.expand("%:t"), vim.fn.expand("%:e"), { default = true })
 		else
 			ft = vim.bo.filetype
 		end
 
 		local color = ""
-		if icon_highlight then
-			local highlight_color = vim.api.nvim_get_hl(0, { name = icon_highlight })
+		if ft_highlight then
+			local highlight_color = vim.api.nvim_get_hl(0, { name = ft_highlight })
 			color = highlight_color.foreground
 		end
 
