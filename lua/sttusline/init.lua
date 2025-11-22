9594911403b7a2982ae2320664355bbfcca6b140
Tran Vo Son Tung <sontungexpt@gmail.com>
2025-11-22 09:09:03 +0700

    notify archived

---
+++ lua/sttusline/init.lua
@@ -0,0 +1,21 @@
+local config = require("sttusline.config")
+local runner = require("sttusline.runner")
+local command = require("sttusline.command")
+
+local M = {}
+
+M.setup = function(user_opts)
+	local opts = config.setup(user_opts)
+	command.setup(opts)
+	runner.setup(opts)
+	vim.defer_fn(
+		function()
+			require("sttusline.utils.notify").warn(
+				"This `sttusline` is now achieved please move to `witch-line` to get better experience"
+			)
+		end,
+		2000
+	)
+end
+
+return M
